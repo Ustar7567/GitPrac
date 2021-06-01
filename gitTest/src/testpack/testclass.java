59dde50be866f5868d3d91f8e68c6373711c61f0
Admin <ustar7567@gmail.com>
2021-06-01 12:28:05 +0530

    Making changes from Eclipse

--- gitTest/src/testpack/testclass.java
+++ gitTest/src/testpack/testclass.java
@@ -5,7 +5,7 @@ public class testclass {
 	public static void main(String[] args) {
 		System.out.println("Practicing Git and Github");
 		System.out.println("Making this change from Github");
-		
+		System.out.println("Adding this from Eclipse");
 
 	}
 
