f311ed2c17de7ec50ef585e28d5a38809f79bcd2
Admin <ustar7567@gmail.com>
2021-06-01 12:49:54 +0530

    Adding a space to Br1 statement

--- gitTest/src/testpack/testclass.java
+++ gitTest/src/testpack/testclass.java
@@ -6,7 +6,7 @@ public class testclass {
 		System.out.println("Practicing Git and Github");
 		System.out.println("Making this change from Github");
 		System.out.println("Adding this from Eclipse");
-		System.out.println("Br1");
+		System.out.println("Br1 ");
 
 	}
 
